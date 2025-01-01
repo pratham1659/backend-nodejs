8a350585b9cc6f58235ae8ae06b6194954404952
prathamkumar <prathamkumar1985@gmail.com>
2025-01-02 01:50:08 +0530

    mongoose chapter added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const morgan = require("morgan");
-const productRouter = require("./routes/product");
+const bookRouter = require("./routes/book");
 const userRouter = require("./routes/user");
-
 const server = express();
+
+//db connect
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/bookly_db");
+  console.log("Database Connected!!");
+}
 
 // bodyParser - middleware
 server.use(express.json());
 server.use(morgan("default"));
 server.use(express.static("temp")); //path middleware to get the files
-server.use("/products", productRouter.routes);
+server.use("/books", bookRouter.routes);
 server.use("/users", userRouter.routes);
 
 // MVC Model-View-Controller
