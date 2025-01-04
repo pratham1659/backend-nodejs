131a47fc48cb6dec3474ad689ff709fd38b715a6
prathamkumar <prathamkumar1985@gmail.com>
2025-01-04 15:50:58 +0530

    vercel major changes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./config/dbConfig");
 const bookRouter = require("./routes/book");
-const userRouter = require("./routes/user");
 const path = require("path");
 const server = express();
 const logger = require("./middlewares/logger");
@@ -21,7 +19,6 @@ server.use(express.json());
 // server.use(express.static("temp")); //path middleware to get the files
 server.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
 server.use("/books", bookRouter.routes);
-server.use("/users", userRouter.routes);
 server.use("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
