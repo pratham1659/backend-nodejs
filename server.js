fceac892e730be8c41fa8a727e97a75ba28d6d01
prathamkumar <prathamkumar1985@gmail.com>
2025-01-12 23:38:06 +0530

    file config fixes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./config/dbConfig");
 const bookRouter = require("./routes/book");
-const path = require("path");
 const server = express();
 const logger = require("./middlewares/logger");
 require("dotenv").config();
@@ -17,11 +16,7 @@ dbConnect().catch((err) => {
 server.use(cors());
 server.use(express.json());
 // server.use(express.static("temp")); //path middleware to get the files
-server.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
 server.use("/books", bookRouter.routes);
-server.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
-});
 
 // Server
 server.listen(8080, () => {
