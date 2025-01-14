3805beeda90a97bd5fc9d9ae7e793a39d4e2985b
prathamkumar <prathamkumar1985@gmail.com>
2025-01-14 16:38:34 +0530

    add protectRoute in auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { signup, login, logout } = require("../controller/authController");
-// const { protectRoute } = require("../middleware/protectRoute");
+const { getMe, signup, login, logout } = require("../controller/authController");
+const protectRoute = require("../middlewares/protectRoute");
 
 const router = express.Router();
 
-// router.get("/getme", protectRoute, getMe);
+router.get("/getme", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
