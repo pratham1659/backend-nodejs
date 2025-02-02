026ff7afc7a52a8fb4017cd127293be1d4b792d3
prathamkumar <prathamkumar1985@gmail.com>
2025-02-03 01:24:50 +0530

    Cookie concept added

--- Concept/10-Expressjs/index.js
+++ Concept/10-Expressjs/index.js
@@ -2,18 +2,22 @@ const express = require("express");
 const userRoutes = require("./routes/UserRoutes");
 const productRoutes = require("./routes/ProductRoutes");
 const loggingMiddleware = require("./middleware");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(express.json());
+app.use(cookieParser("authKey"));
 app.use(loggingMiddleware);
 app.use(userRoutes);
 app.use(productRoutes);
 
-app.get("/", (res, req) => {
-  res.send("Welcome to Homepage");
+app.get("/get-cookie", (req, res) => {
+  res.cookie("key", "JohnDoe", { maxAge: 60000, signed: true });
+  console.log(req.cookies);
+  res.send("Cookie has been set!");
 });
 
 const port = 3000;
--- Concept/10-Expressjs/routes/ProductRoutes.js
+++ Concept/10-Expressjs/routes/ProductRoutes.js
@@ -3,7 +3,14 @@ const router = Router();
 const products = require("../utils/constants");
 
 router.get("/api/products", (req, res) => {
-  res.json(products);
+  console.log(req.cookies);
+  console.log(req.signedCookies);
+
+  if (req.signedCookies.key && req.signedCookies.key === "JohnDoe") {
+    return res.json(products);
+  }
+
+  return res.send({ msg: "Sorry. You need the correct cookie" });
 });
 
 module.exports = router;
